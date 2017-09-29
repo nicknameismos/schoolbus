e2a966ef2c534226f407037325848eb7e1d07523
nicknameismos <mynameissarawut@gmail.com>
2017-09-30 00:26:21 +0700

    login

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { LoginPage } from './../pages/login/login';
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { Facebook } from '@ionic-native/facebook';
+import { EcommerceCoreModule, AuthenService } from '@ngcommerce/core';
+import { HttpModule } from '@angular/http';
+
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    LoginPage
+  ],
+  imports: [
+    BrowserModule,
+    EcommerceCoreModule.forRoot('https://school-bus-server.herokuapp.com/api/'),
+    HttpModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    LoginPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    Facebook,
+    AuthenService,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
+  ]
+})
+export class AppModule { }
---
+++ src/pages/login/login.ts
@@ -0,0 +1,100 @@
+import { HomePage } from './../home/home';
+import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, Slides } from 'ionic-angular';
+import { AuthenService, SignupModel, SigninModel } from '@ngcommerce/core';
+
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+  dataUser: any = {
+    firstName: '',
+    lastName: '',
+    tel: '',
+    email: '',
+    username: '',
+    password: ''
+  };
+
+  credentail: any = {};
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private fb: Facebook,
+    public authenService: AuthenService
+  ) {
+  }
+  @ViewChild(Slides) slides: Slides;
+
+
+  // slide() {
+  //   this.slides.slideTo(2, 500);
+  // }
+  loginFacebook() {
+    this.fb.login(['public_profile', 'email'])
+      .then((res: FacebookLoginResponse) =>
+        this.fb.api('me?fields=email,id,first_name,last_name', null).then((res: FacebookLoginResponse) =>
+          this.registerFb(res))
+          .catch(e => {
+            alert(JSON.stringify(e));
+          })
+      )
+      .catch(e => {
+        alert(JSON.stringify(e));
+      });
+
+  }
+  registerFb(data) {
+    // alert(JSON.stringify(data));
+    this.dataUser.firstName = data.first_name;
+    this.dataUser.lastName = data.last_name;
+    this.dataUser.email = data.email;
+    this.dataUser.username = data.email;
+    this.slides.slideTo(1, 500, this.dataUser);
+
+  }
+  submit(dataUser) {
+    console.log(this.dataUser);
+    this.authenService.signUp(this.dataUser).then((data) => {
+      this.navCtrl.push(HomePage);
+      this.dataUser = '';
+      
+    }, (error) => {
+      console.error(error);
+      alert(JSON.stringify(error));
+    })
+  }
+
+  logIn() {
+    this.credentail.username = this.dataUser.email;
+    this.credentail.password = this.dataUser.password;
+
+    this.authenService.signIn(this.credentail).then((data) => {
+      console.log(data);
+      this.navCtrl.push(HomePage);
+
+    }, (error) => {
+      console.error(error);
+      alert(JSON.stringify(error));
+    })
+  }
+
+  next(dataUser) {
+    console.log(dataUser);
+    this.slides.slideTo(2, 500, dataUser);
+  }
+  register(){
+    this.slides.slideTo(1, 500);
+  }
+}
